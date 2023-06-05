2151417746aa1a3d7bf9a28ded9fb32e2045ef84
donatoway <donatoway@gmail.com>
2023-06-05 15:30:02 +0200

    done11

--- src/pages/Excursions/Excursions-jetski/index.tsx
+++ src/pages/Excursions/Excursions-jetski/index.tsx
@@ -7,9 +7,61 @@ import { ResponsiveAppBar } from "@/components/app-bar";
 import { Card } from "@mui/material";
 import { useRouter } from "next/router";
 import  Router  from "next/router";
-import jetskiList from "./jetskiList";
 import {Whatsapp} from "@/components/whatsapp"
 /* eslint-disable import/no-anonymous-default-export */
+
+const jetskiList = [
+  {
+      key: 'puerta',
+      name: "JETSKI TOUR TO PUERTAS DEL CIELO",
+      priceHighSeason: 160,
+      priceLowSeason: 180,
+      description: "Getting to know the most beautiful corners of Ibiza can sometimes be a bit difficult, so with our jet skis you will be able to discover the secrets that the wonderful island of Ibiza hides . Go ahead and enjoy an adventure full of adrenaline and speed through the crystal clear waters of the Mediterranean .From there one hour will begin where we will visit Isla Margarita , one of the most beautiful islets in Ibiza and a great place to visit caves that are only They can access by jet ski . In addition, we will also visit the spectacular Cala Gracia and Cala Salada with cliffs that will leave you speechless.We are at the disposal of 10 two-seater jet skis , which means that if you want to share this experience with someone, you can get on the jetski together. To drive the jet ski it is not necessary to have a license and therefore you will always be accompanied by a qualified monitor who will guide you at all times through the journey. You can only drive motorcycles if you are over 16 years old ; But if you are under that age do not worry, you can go as a co-pilot and also enjoy this incredible experience.",
+      picture: "/Jetski/jetski-puerta/jetski-puerta3.png",
+      images: [   "/Jetski/jetski-puerta/jetski-puerta.png",
+      "/Jetski/jetski-puerta/jetski-puerta2.png",
+      "/Jetski/jetski-puerta/jetski-puerta3.png",
+      "/Jetski/jetski-puerta/jetski-puerta4.png"
+  
+  ],
+  },
+  {
+      key: 'vedra',
+      name: "JETSKI TOUR TO ES VEDRA",
+      priceHighSeason: 200,
+      priceLowSeason: 240,
+      description: "If Ibiza is known for something, apart from its incredible parties, it is for the spectacular Mediterranean that surrounds it. Dare to take a jet ski ride through the most famous island of Ibiza , Es Vedrá , where you can discover places that if it weren’t for The jetski , you could not know while one of our monitors guides you through its best coves . Our professional instructors will be waiting for you, who will be in charge of teaching you the regulations and guiding you along the route. We have 10 two-seater jet skis so you can share this experience with your family, partner and friends. Do not worry if you do not have a license since we take care of that, our monitors will be your assistants and will teach you how to handle our incredible jet skis. You will visit the wonderful natural reserve of Es Vedrá for 1 hour and a half and if you come with the children of the house you should know that if they are under 16 years of age they cannot drive the bike but they can enjoy the experience as co-drivers.",
+      picture: "/Jetski/jetski-vedra/jetski-vedra.png",
+      images: ["/Jetski/jetski-vedra/jetski-vedra.png",
+      "/Jetski/jetski-vedra/jetski-vedra2.png",
+      "/Jetski/jetski-vedra/jetski-vedra3.png"],
+  },
+  {
+      key: 'cala-comte',
+      name: "JETSKI TOUR CALA CONTA",
+      priceHighSeason: 140,
+      priceLowSeason: 165,
+      description: "If you are passionate about adrenaline and speed , drive a jet ski through the crystal clear waters of Ibiza > you are going to love it. Come and discover the unknown caves in the middle of the Mediterranean and the most beautiful coves of the Ibizan island with us and our amazing jetski , you won’t be disappointed! Our instructors will be waiting for you. He will be the one who will guide you on the jet ski route and who will teach you how to sail the seas with a boat as fun as this one. We have available up to 10 jet skis with capacity for a maximum of 2 people each, so if you were thinking of sharing this adventure with a friend, a family member or your partner , our jet skis are the ones indicated. To carry out the route it is not necessary to have a nautical license so you will always be accompanied by a monitor who will give you instructions throughout the tour. The minimum age to drive the jetski is 16 years old so, if you are under that age, you can always go on the excursion accompanied by an adult.",
+      picture: "/Jetski/jetski-conta/jetskiConta0.png",
+      images: [   "/Jetski/jetski-conta/jetskiConta0.png",
+                  "/Jetski/jetski-conta/jetskiConta1.png",
+                  "/Jetski/jetski-conta/jetskiConta2.png",
+                  "/Jetski/jetski-conta/jetskiConta3.png"
+              ],
+  },
+  {
+      key: 'atlantis',
+      name: "JETSKI TOUR TO ATLANTIS",
+      priceHighSeason: 240,
+      priceLowSeason: 280,
+      description: "Surely you have ever heard about Atlantis and how difficult it is to access it. That is why we offer you a jet ski route through the waters of Ibiza so that you can discover the hidden cove of Atlantis and discover all the secrets that it hides. For two hours you will visit the crystal clear waters and rock formations that the Ibiza coast presents, but the star destination of this excursion is Atlantis, the well-known cove secret of Ibiza . You will enjoy the magic of this cove and its natural pools of crystalline color. It will be a ride in jetski that you will never forget since the experience of knowing one of the most visited rock quarries in Ibiza does not pass every days. The instructors teach you the keys necessary to cross the Ibizan waters at full speed. We have 10 jetskis so if you were thinking of sharing this experience with someone, this is the right place for you. In addition, you do not need to have a license since the monitors will accompany you at all times and will be in charge of guiding you through this adventure. Of course, to be able to drive the jetski you have to be over 16 years old , otherwise you can go as a co-pilot and also enjoy the experience.",
+      picture: "/Jetski/jetski-atlantis/jetski-atlantis.png",
+      images: ["/Jetski/jetski-atlantis/jetski-atlantis.png",
+              "/Jetski/jetski-atlantis/jetski-atlantis2.png",
+              "/Jetski/jetski-atlantis/jetski-atlantis3.png",
+              "/Jetski/jetski-atlantis/jetski-atlantis4.png"],
+  },
+]
 
 function sendProp (obj:any)
 {
@@ -41,5 +93,3 @@ export default function ()
         </div>
     )
 }
-
-export {jetskiList}
--- src/pages/SunsetTrip/SunsetTrip-index/index.tsx
+++ src/pages/SunsetTrip/SunsetTrip-index/index.tsx
@@ -9,9 +9,35 @@ import { ResponsiveAppBar } from "@/components/app-bar";
 import { Card } from "@mui/material";
 import { useRouter } from "next/router";
 import  Router  from "next/router";
-import sunsetList from "../sunsetList";
 import { Whatsapp } from "@/components/whatsapp";
 /* eslint-disable import/no-anonymous-default-export */
+
+const sunsetList = [
+  {
+      key: 'Sunset Tour',
+      name: "Sunset Tour",
+      price: 70,
+      hours: "3 hours before sunset",
+      description: "This magnificent excursion in capital letters is designed for all audiences, from families to couples, it is the ideal activity for those explorers who want to visit the most beautiful, wild and remote places on the coast of Ibiza, being able to swim, snorkel and paddle surf. In the most crystalline waters of the island of Ibiza. We will sail from the port of San Antonio and head west, to the underwater quarry. From there, we will sail to Cala Conta, during approximately 20 minutes of navigation we will be able to observe the majestic beauty of the coast with its coves of pristine waters in the that if the state of the sea allows it, they will be shown the small caves that this coast has.",
+      listDescription: ["We depart 3 hours before sunset, due to the sundown time changes, we will inform you one time you book, but the times are really close to booking times shown.",
+      "Valid for  and non-swimmers (special life jacket is provided).",
+      "Visiting two of the most beautiful beaches and sea caves in Ibiza.",
+      "You will see the sunset with a glass of cava from our boat in front of CAFE MAMBO",
+      "Unlimited drinks included. (Cava, beer, sangria, Fanta, water).",
+      "Daily departures.",
+      "Duration of 3 hours (1 swim stops + 1 to watch the sunset)",
+      "Meeting point in the port of San Antonio.",
+      "Includes snorkel equipment (mask and tube), 6 SUP Paddles on board. 1 Relaxing sea foam mattress",
+      "Perfect excursion during your holidays to explore the coast of Ibiza and admire the underwater beauty of Ibiza with everything included.",
+      "Highly recommended for a group of friends, couples or an incredible celebration."],
+      picture: "/Sunset/trip70euro.jpeg",
+      images: ["/Sunset/trip70euro.jpeg",
+      "/Sunset/trip70euro2.jpeg",
+      "/Sunset/trip70euro3.jpeg",
+      "/Sunset/trip70euro4.jpeg",
+  ],
+  },
+]
 
 
 function sendProp (obj:any)
