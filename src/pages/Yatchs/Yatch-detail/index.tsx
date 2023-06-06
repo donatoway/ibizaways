1d1e79d0bac4b4ad8205fa1fb566e338205e0274
donatoway <donatoway@gmail.com>
2023-06-06 11:50:57 +0200

    done24

--- src/pages/Yatchs/Yatch-detail/index.tsx
+++ src/pages/Yatchs/Yatch-detail/index.tsx
@@ -1,10 +1,58 @@
-/* eslint-disable react/display-name */
 /* eslint-disable import/no-anonymous-default-export */
-import Detail from "./detail"
+/* eslint-disable react/display-name */
+import { ActionAreaCard } from "@/components/Card";
+import { ResponsiveAppBar } from "@/components/app-bar";
+import Image from "next/image";
+import { useEffect } from 'react';
+import { useRouter } from "next/router";
+import {Contact} from "@/components/contactEmail"
+import {Whatsapp} from "@/components/whatsapp"
 
-export default function ()
+
+
+export default function  () 
 {
+    const router = useRouter()
+    const {
+      query : {name, description, picture, images, info}
+    } = router;
+   
+    const props = 
+    {
+      name,
+      description,
+      picture,
+      images,
+      info
+    }
+
     return (
-            <Detail></Detail>
-    )
-}
+      <div>
+            <div>
+              <ResponsiveAppBar />
+                <h1 style={{textAlign:"center"}}>{props.name}</h1>
+            </div>
+
+        {
+          (info as string[]).map((p) => 
+          (
+            // eslint-disable-next-line react/jsx-key
+            <div  style={{display:"flow", marginTop:"20%", textAlign:"center", width:"100%", height:"100%"}}>
+              <Image
+              src={p}
+              width={'300'}
+              height={'450'}
+              alt="Picture of the author"
+              style={{display:"flex", width:"100%", height:"100%"}}
+              
+            />
+             
+            </div>
+
+          ))
+          }
+        <Whatsapp></Whatsapp>
+      <Contact></Contact>
+      </div>
+    ) 
+  }
